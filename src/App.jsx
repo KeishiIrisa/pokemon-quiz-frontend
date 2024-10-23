d7f1673bff7665895391f50933ee9cc64d912756
KeishiIrisa <keishi.irisa@gmail.com>
2024-10-23 13:38:59 +0900

    changed handleFetchPokemon function to useEffect function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { X } from "lucide-react";
 
 
@@ -13,6 +13,8 @@ const fetchPokemon = async (id) => {
   return await response.json();
 }
 
+
+// pokemonSpecies includes description about the pokemon such as ""This POKéMON has electricity-storing\npouches on its cheeks. These appear to\nbecome electrically charged during the\fnight while PIKACHU sleeps.\nIt occasionally discharges electricity\nwhen it is dozy after waking up.""
 const fetchPokemonSpecies = async (id) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${id}`
@@ -20,58 +22,95 @@ const fetchPokemonSpecies = async (id) => {
   return await response.json();
 }
 
+
+// pokemonAbility includes the description of ability the pokemon has.
+const fetchPokemonAbilityDescription = async (abilities) => {
+  const abilitiesDatas = [];
+
+  for (const ability of abilities) {
+    const response = await fetch(ability.ability.url);
+    const data = await response.json()
+    abilitiesDatas.push(data);
+  }
+  
+  return abilitiesDatas;
+}
+
+
+// pokemon description is array object, so it includes various language description, so this function select two english and japanese sentences which has same meaning.
+const selectPokemonDescription = (updatedPokemon) => {
+  if (updatedPokemon.pokespecies.flavor_text_entries) {
+    // getting japanese flavor text
+    const flavors_ja = updatedPokemon.pokespecies.flavor_text_entries
+    .filter(entry => entry.language.name === 'ja');
+    // getting english flavor text
+    const flavors_en = updatedPokemon.pokespecies.flavor_text_entries
+    .filter(entry => entry.language.name === 'en');
+
+    const pokemonDescriptions = flavors_ja.map(entry_ja => {
+      const entry_en = flavors_en.find(entry_en => entry_en.version.name === entry_ja.version.name);
+      if (entry_en) {
+        return {
+          ja: entry_ja,
+          en: entry_en,
+          version: entry_en.version.name
+        };
+      }
+      return null;
+    }).filter(entry => entry !== null);
+    
+    return pokemonDescriptions[0]
+  };
+}
+
 function App() {
+  const [pokeId, setPokeId] = useState(null);
   const [pokemon, setPokemon] = useState(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  const selectPokemonDescription = (updatedPokemon) => {
-    if (updatedPokemon.pokespecies.flavor_text_entries) {
-      // getting japanese flavor text
-      const flavors_ja = updatedPokemon.pokespecies.flavor_text_entries
-      .filter(entry => entry.language.name === 'ja');
-      // getting english flavor text
-      const flavors_en = updatedPokemon.pokespecies.flavor_text_entries
-      .filter(entry => entry.language.name === 'en');
-
-      const pokemonDescriptions = flavors_ja.map(entry_ja => {
-        const entry_en = flavors_en.find(entry_en => entry_en.version.name === entry_ja.version.name);
-        if (entry_en) {
-          return {
-            ja: entry_ja,
-            en: entry_en,
-            version: entry_en.version.name
-          };
-        }
-        return null;
-      }).filter(entry => entry !== null);
-      
-      return pokemonDescriptions[0]
-    };
+  const [popupData, setPopupData] = useState(null);
+  /*
+  interface PopupData {
+    popupTitle: string;
+    popupContent: string;
   }
+  */
 
-  // fetchPokemon and fetchPokemonSpecies
-  const handleFetchPokemonDetails = async (e) => {
-    e.preventDefault();
-    const formdata = new FormData(e.target);
-    const poke_id = formdata.get("poke_id")
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      if (!pokeId) return;
 
     // exucute both fetch requests simultaneously
     const [pokemonData, speciesData] = await Promise.all([
-      fetchPokemon(poke_id),
-      fetchPokemonSpecies(poke_id)
-    ])
+      fetchPokemon(pokeId),
+      fetchPokemonSpecies(pokeId)
+    ]);
 
     const updatedPokemon = {
       ...pokemonData,
       pokespecies: speciesData
     }
 
+    const abilitiesDatas = await fetchPokemonAbilityDescription(updatedPokemon.abilities);
+
     const pokedescriptions = selectPokemonDescription(updatedPokemon);
 
     setPokemon({
       ...updatedPokemon,
-      pokedescriptions: pokedescriptions
-    })
+      pokedescriptions: pokedescriptions,
+      pokeabilities: abilitiesDatas
+    });
+    };
+
+    fetchPokemonDetails();
+  }, [pokeId])
+
+
+  const handleSetPokeId = async (e) => {
+    e.preventDefault();
+    const formdata = new FormData(e.target);
+    const poke_id = formdata.get("poke_id")
+    setPokeId(poke_id);
   }
 
   const togglePopup = () => {
@@ -83,11 +122,11 @@ function App() {
     <div className="bg-gray-100 min-h-screen pt-16 pb-8 space-y-8">
       {/* Hero Section */}
       <div className="text-center space-y-8">
-        <h1 className="text-5xl font-bold">Pokemon Quiz</h1>
+        <h1 className="text-5xl font-bold">Learn English With Pokemon!</h1>
         <p className="text-gray-700">
           Enjoy pokemon quiz!
         </p>
-        <form onSubmit={handleFetchPokemonDetails}><input name="poke_id"/><button type="submit">Search</button></form>
+        <form onSubmit={handleSetPokeId}><input name="poke_id"/><button type="submit">Search</button></form>
       </div>
       {/* quiz card */}
       <div className="flex justify-center">
@@ -124,12 +163,21 @@ function App() {
           {/* 特性 */}
           <div className="flex items-center">
             <h2>特性：</h2>
-            {pokemon && pokemon.abilities.map((ability, index) => (
+            {pokemon && pokemon.pokeabilities.map((ability, index) => (
               <>
                 <span key={index} className="ml-2">
-                  {ability.ability.name}
+                  {ability.name}
                 </span>
-                <button onClick={togglePopup}>特性❔</button>
+                {/* TODO: do not execute setPopupData on clicking element, execute this when handleFethPokemon is executed */}
+                <button onClick={() => {
+                  const flavor_en = ability.flavor_text_entries.filter(entry => entry.language.name === 'en')[0] || null;
+                  if (flavor_en) {
+                    setPopupData({
+                      popupTitle: ability.name,
+                      popupContent_en: flavor_en.flavor_text
+                    }) ;         
+                  }
+                }}>特性❔</button>
               </>
 
             ))}
@@ -145,19 +193,19 @@ function App() {
         </div>
       </div>
       {/* ポップアップカード */}
-      {isPopupOpen && (
+      {isPopupOpen && popupData && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" onClick={togglePopup}>
           {/* シャドー */}
           <div className="bg-white rounded-xl shadow-md  w-[700px]">
             {/* hero_popup */}
             <div className="bg-gray-300 rounded-tl-xl rounded-tr-xl pt-2 pr-2 pl-2 pb-2 flex items-center justify-between">
-              <h2>シンクロ</h2>
+              <h2>popupData.popupTitle</h2>
               <button onClick={togglePopup} className="flex">
                 <X></X>
               </button>
             </div>
             {/* content_popup */}
-            <div className="p-2">this is a content</div>
+            <div className="p-2">{popupData.popupContent_en}</div>
           </div>
         </div>
       )}
