71dd3229ced205ad2136116569ceffcb899c04ce
KeishiIrisa <keishi.irisa@gmail.com>
2024-10-29 00:53:46 +0900

    changed config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "pokemon-quiz-frontend" : "./",
   plugins: [react()],
-  server: {
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline';",
-      "Permissions-Policy":
-        "geolocation=(self), microphone=(), camera=(), fullscreen=(), payment=()",
-    },
-  },
 });
